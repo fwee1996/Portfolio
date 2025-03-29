b84b5a0b84109437340f9e44f88347611d247d52
fwee1996 <70270753+fwee1996@users.noreply.github.com>
2025-03-28 23:06:53 -0400

    Updates

--- src/Home.js
+++
@@ -1,8 +0,0 @@
-export const Home = () => {
-    return (
-        <div>
-        <h1>Welcome to Fiona's Portfolio</h1>
-        <p>This is the home page of my portfolio.</p>
-        </div>
-    );
-    }
