bdf9eea97b3283e86cdaf2f4e3a9b7e36197dc14
fwee1996 <70270753+fwee1996@users.noreply.github.com>
2025-03-28 22:17:03 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Home } from './Home';
+// import About from './About';
+// import Projects from './Projects';
+// import Contact from './Contact';
+// import { useState } from 'react';
+// import { useEffect } from 'react';
+// import { useRef } from 'react'; 
 
 function App() {
   return (
     <div className="App">
+      <Home/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </p> */}
+      <img className="cat-pic" src="https://i.pinimg.com/474x/57/23/c8/5723c86aed5d1c9db9d74c3d8b7aa347.jpg" alt="cat-image"></img>
+      <img src="https://media.tenor.com/RdCh8-EI1mEAAAAj/dance.gif"></img>
+        <a className="cat-pic-link" href="https://i.pinimg.com/474x/57/23/c8/5723c86aed5d1c9db9d74c3d8b7aa347.jpg"> Meet Kitty </a>
       </header>
     </div>
   );
---
+++ src/Home.js
@@ -0,0 +1,8 @@
+export const Home = () => {
+    return (
+        <div>
+        <h1>Welcome to Fiona's Portfolio</h1>
+        <p>This is the home page of my portfolio.</p>
+        </div>
+    );
+    }
